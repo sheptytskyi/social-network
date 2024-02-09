6a50b01c0a16e0d138f45c0b5943368aad16ee15
Dima Sheptytskyi <channektoshka@gmail.com>
2024-02-09 21:47:44 +0200

    feat: implement user auth

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
+import uuid
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+
+from app import User
+from app.internal.auth import auth_backend
+from app.managers.user import get_user_manager
+from app.schemas.user import UserRead, UserUpdate, UserCreate
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI()
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 
 @app.get("/")
--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,9 @@
-from database.db import Base, engine
+from database.db import Base, engine, get_async_session
+from app.models.user import User
 
 __all__ = (
     'Base',
     'engine',
+    'get_async_session',
+    'User'
 )
